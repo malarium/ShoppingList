19d23f42fd15bd45a38e93b24b2d053767fa161a
Marcin <marcingorecki1981@gmail.com>
2019-04-23 23:27:32 +0200

    Starter

---
+++ scripts.js
@@ -0,0 +1,48 @@
+new Vue({
+  el: "#app",
+  data: {
+    newItem: "",
+    showList: true,
+    mockItems: ['ser', 'mleko', 'woda', 'chleb', 'bułki'],
+    // mockItems: [],
+    boughtItems:['wino', 'zapałki', 'serwetki'],
+    // boughtItems: []
+  },
+  computed: {
+    itNr: function() {
+      return this.mockItems.length;
+    },
+    bgNr: function() {
+      return this.boughtItems.length;
+    }
+  },
+  methods: {
+    submitted: function() {
+      if (this.newItem != "") {
+        this.mockItems.push(this.newItem);
+        this.newItem = "";
+      }
+    },
+    removeAll: function() {
+      this.mockItems = [];
+    },
+    removeAllBought: function() {
+      this.boughtItems = [];
+    },
+    removeThis: function(el) {
+      this.mockItems.splice(this.mockItems.indexOf(el), 1);
+    },
+    haveThis: function(el) {
+      this.boughtItems.push(el);
+      this.removeThis(el);
+    },
+    removeBought: function(el) {
+      this.boughtItems.splice(this.boughtItems.indexOf(el), 1);
+      this.newItem = el;
+      this.submitted();
+    },
+    toggleVisibility: function() {
+      this.showList = !this.showList;
+    }
+  }
+});
